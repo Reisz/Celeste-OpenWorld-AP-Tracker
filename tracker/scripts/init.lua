a7561fd3c003e9ceb616a414bc6a3e2263151dc4
Reisz <Reisz@users.noreply.github.com>
2026-01-09 09:17:16 +0100

    Add interactables

--- tracker/scripts/init.lua
+++ tracker/scripts/init.lua
@@ -1,6 +1,9 @@
 Tracker:AddMaps("maps/maps.json")
 
+Tracker:AddItems("items/interactables.json")
+
 Tracker:AddLayouts("layouts/maps.json")
+Tracker:AddLayouts("layouts/interactables.json")
 Tracker:AddLayouts("layouts/tracker.json")
 
 Tracker:AddLocations("locations/berries.json")
