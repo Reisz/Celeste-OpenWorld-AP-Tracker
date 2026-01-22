94c690824bca5db81cc9feb403d6371ff1437b99
Reisz <Reisz@users.noreply.github.com>
2026-01-22 21:05:07 +0100

    Map berry names to randomizer names
    At least as far as easily determinable.

--- generator/generate_locations.py
+++ generator/generate_locations.py
@@ -6,6 +6,57 @@ HEART_COLORS = {
     "c": "yellow",
 }
 
+BERRY_MAPPING = {
+    # https://berrycamp.github.io/map/celeste/site/a?room=d2
+    "site_d2_9": 2,
+    "site_d2_31": 1,
+    # https://berrycamp.github.io/map/celeste/resort/a?room=s2
+    "resort_s2_6": 2,
+    "resort_s2_18": 1,
+    # https://berrycamp.github.io/map/celeste/resort/a?room=03-b
+    "resort_03-b_1": 2,
+    "resort_03-b_25": 1,
+    # https://berrycamp.github.io/map/celeste/resort/a?room=roof06
+    "resort_roof06_276": 1,  # TODO: Order not clear from level data
+    "resort_roof06_308": 2,
+    # https://berrycamp.github.io/map/celeste/ridge/a?room=b-01
+    "ridge_b-01_6": 1,  # TODO: Order not clear from level data
+    "ridge_b-01_13": 2,
+    # https://berrycamp.github.io/map/celeste/ridge/a?room=b-02
+    "ridge_b-02_20": 1,
+    "ridge_b-02_58": 2,
+    # https://berrycamp.github.io/map/celeste/temple/a?room=a-01
+    "temple_a-01_164": 1,  # TODO: Order not clear from level data
+    "temple_a-01_256": 2,
+    # https://berrycamp.github.io/map/celeste/temple/a?room=b-17
+    "temple_b-17_10": 2,
+    "temple_b-17_14": 1,
+    # https://berrycamp.github.io/map/celeste/temple/a?room=b-20
+    "temple_b-20_72": 1,
+    "temple_b-20_183": 2,
+    # https://berrycamp.github.io/map/celeste/temple/a?room=d-04
+    "temple_d-04_16": 1,
+    "temple_d-04_122": 2,
+    # https://berrycamp.github.io/map/celeste/temple/a?room=d-15
+    "temple_d-15_217": 2,
+    "temple_d-15_335": 1,
+    # https://berrycamp.github.io/map/celeste/summit/a?room=a-04b
+    "summit_a-04b_85": 2,
+    "summit_a-04b_136": 1,
+    # https://berrycamp.github.io/map/celeste/summit/a?room=f-11
+    "summit_f-11_1068": 2,  # TODO: Order between 1 and 2 not clear from level data
+    "summit_f-11_1229": 3,
+    "summit_f-11_1238": 1,
+    # https://berrycamp.github.io/map/celeste/summit/a?room=g-00b
+    "summit_g-00b_37": 1,
+    "summit_g-00b_114": 3,
+    "summit_g-00b_127": 2,
+    # https://berrycamp.github.io/map/celeste/summit/a?room=g-01
+    "summit_g-01_66": 1,
+    "summit_g-01_279": 3,  # TODO: Order between 2 and 3 not clear from level data
+    "summit_g-01_342": 2,
+}
+
 data = json.load(open("data/celeste.json"))
 
 locations_children = []
@@ -34,8 +85,9 @@ for chapter in data["chapters"]:
                     }
 
                 berry_count = len(room["entities"].get("berry", []))
-                for index, entity in enumerate(room["entities"].get("berry", [])):
-                    suffix = f" {index + 1}" if berry_count > 1 else ""
+                for entity in room["entities"].get("berry", []):
+                    berry_id = f"{chapter['id']}_{room_id}_{entity['id']}"
+                    suffix = f" {BERRY_MAPPING[berry_id]}" if berry_count > 1 else ""
                     locations_children.append(
                         {
                             "name": f"{chapter['name']} {side['name']} [Room {room_id}] Strawberry{suffix}",
