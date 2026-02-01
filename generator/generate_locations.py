55b8647edc549678a793b92ec2ff0b6f3c23b0f0
Reisz <Reisz@users.noreply.github.com>
2026-02-01 16:41:39 +0100

    Calculate rules for strawberries, cassetes and hearts

--- generator/generate_locations.py
+++ generator/generate_locations.py
@@ -1,5 +1,7 @@
 import json
 import pathlib
+from collections import defaultdict
+from dataclasses import dataclass
 
 HEART_COLORS = {
     "a": "blue",
@@ -7,6 +9,22 @@ HEART_COLORS = {
     "c": "yellow",
 }
 
+# Berrycamp `<chapter>` -> AP chapter index
+CHAPTER_MAP = {
+    "prologue": 0,
+    "city": 1,
+    "site": 2,
+    "resort": 3,
+    "ridge": 4,
+    "temple": 5,
+    "reflection": 6,
+    "summit": 7,
+    "epilogue": 8,
+    "core": 9,
+    "farewell": 10,
+}
+
+# Berrycamp `<chapter>_<room>_<berry>` -> AP berry index
 BERRY_MAPPING = {
     # https://berrycamp.github.io/map/celeste/site/a?room=d2
     "site_d2_9": 2,
@@ -61,6 +79,165 @@ BERRY_MAPPING = {
 data = json.load(open("data/celeste.json"))
 ids = json.load(open("data/ids.json"))["location_name_to_id"]
 
+
+@dataclass(frozen=True)
+class ApRule:
+    rule: frozenset[frozenset[str]] = frozenset()
+
+    @staticmethod
+    def from_ap(rule: list[list[str]]) -> "ApRule":
+        if not rule:
+            return ApRule()
+        return ApRule(frozenset(frozenset(group) for group in rule))
+
+    def to_poptracker(self) -> list[str]:
+        return [",".join(group) for group in self.rule]
+
+    def __and__(self, other: "ApRule") -> "ApRule":
+        if not self.rule:
+            return other
+        if not other.rule:
+            return self
+
+        return ApRule(frozenset(a | b for a in self.rule for b in other.rule))
+
+    def __or__(self, other: "ApRule") -> "ApRule":
+        return ApRule(self.rule | other.rule)
+
+
+@dataclass
+class ApRegionConnection:
+    dest_region: str
+    rule: ApRule
+
+
+@dataclass
+class ApRegion:
+    entity_rules: dict[str, ApRule]
+    region_connections: list[ApRegionConnection]
+    rule: ApRule = ApRule()
+
+
+@dataclass
+class ApRoom:
+    regions: dict[str, ApRegion]
+
+
+@dataclass
+class ApChapter:
+    rooms: dict[str, ApRoom]
+
+
+@dataclass(frozen=True)
+class RegionIndex:
+    room_id: str
+    region_id: str
+
+
+@dataclass(frozen=True)
+class Route:
+    route: tuple[RegionIndex]
+
+    @staticmethod
+    def start(chapter_data) -> "Route":
+        start_room = next(
+            x for x in chapter_data["rooms"] if x["checkpoint"] == "Start"
+        )
+        return Route(
+            (RegionIndex(start_room["name"], start_room["checkpoint_region"]),)
+        )
+
+    def back(self) -> RegionIndex:
+        return self.route[-1]
+
+    def extend(self, region_index: RegionIndex) -> "Route | None":
+        if region_index in self.route:
+            return None
+        return Route(self.route + (region_index,))
+
+    def extended_to_region(self, region_id: str) -> "Route | None":
+        return self.extend(RegionIndex(self.back().room_id, region_id))
+
+
+@dataclass
+class Todo:
+    route: list[RegionIndex]
+    rule: ApRule
+
+
+ap_chapters: dict[str, ApChapter] = {}
+for chapter_data in json.load(open("data/CelesteLevelData.json"))["levels"]:
+    if chapter_data["name"].startswith("10"):
+        continue
+
+    print(f"Calculating rules for {chapter_data['display_name']}")
+
+    rooms = {}
+    ap_chapters[chapter_data["name"]] = ApChapter(rooms=rooms)
+
+    # Convert individual room data
+    for room_data in chapter_data["rooms"]:
+        rooms[room_data["name"]] = ApRoom(
+            {
+                region_data["name"]: ApRegion(
+                    entity_rules={
+                        x["display_name"]: ApRule.from_ap(x["rule"])
+                        for x in region_data.get("locations", [])
+                    },
+                    region_connections=[
+                        ApRegionConnection(
+                            dest_region=x["dest"], rule=ApRule.from_ap(x["rule"])
+                        )
+                        for x in region_data["connections"]
+                    ],
+                )
+                for region_data in room_data["regions"]
+            }
+        )
+
+    # Prepare room connections
+    doors = defaultdict(list)
+    for conn in chapter_data["room_connections"]:
+        doors[RegionIndex(conn["source_room"], conn["source_door"])].append(
+            RegionIndex(conn["dest_room"], conn["dest_door"])
+        )
+
+    # Graph traversal
+    todo = [Todo(Route.start(chapter_data), ApRule())]
+    while todo:
+        current_todo = todo.pop()
+
+        # Add calculated rule to current region
+        rooms[current_todo.route.back().room_id].regions[
+            current_todo.route.back().region_id
+        ].rule |= current_todo.rule
+
+        # Schedule connections within room
+        current_room = rooms[current_todo.route.back().room_id]
+        current_region = current_room.regions[current_todo.route.back().region_id]
+        for conn in current_region.region_connections:
+            route = current_todo.route.extended_to_region(conn.dest_region)
+            if route is not None:
+                todo.append(Todo(route, current_todo.rule & conn.rule))
+
+        # Schedule connections to other rooms
+        for conn in doors[current_todo.route.back()]:
+            route = current_todo.route.extend(conn)
+            if route is not None:
+                todo.append(Todo(route, current_todo.rule))
+
+
+def get_access_rules(chapter, side, room_id, entity):
+    chapter = ap_chapters[f"{CHAPTER_MAP[chapter['id']]}{side['id']}"]
+    region, region_id = next(
+        (region, region_id)
+        for region_id, region in chapter.rooms[room_id].regions.items()
+        if entity in region.entity_rules
+    )
+
+    return (region.rule & region.entity_rules[entity]).to_poptracker()
+
+
 locations_children = []
 locations = [{"children": locations_children}]
 
@@ -96,6 +273,9 @@ for chapter in data["chapters"]:
                     locations_children.append(
                         {
                             "name": f"{chapter['name']} {side['name']} [Room {room_id}] Strawberry{suffix}",
+                            "access_rules": get_access_rules(
+                                chapter, side, room_id, f"Strawberry{suffix}"
+                            ),
                             "map_locations": [loc(entity)],
                             "sections": [{}],
                             "chest_unopened_img": "images/locations/strawberry.png",
@@ -126,6 +306,9 @@ for chapter in data["chapters"]:
                     locations_children.append(
                         {
                             "name": name,
+                            "access_rules": get_access_rules(
+                                chapter, side, room_id, "Cassette"
+                            ),
                             "map_locations": [loc(entity)],
                             "sections": [{}],
                             "chest_unopened_img": "images/locations/cassette.png",
@@ -147,9 +330,17 @@ for chapter in data["chapters"]:
 
                     heart_color = HEART_COLORS[side["id"]]
                     name = f"{chapter['name']} {heart_color.title()} Heart"
+                    ap_name = (
+                        "Crystal Heart"
+                        if side["id"] == "a" and chapter["id"] != "core"
+                        else "Level Clear"
+                    )
                     locations_children.append(
                         {
                             "name": name,
+                            "access_rules": get_access_rules(
+                                chapter, side, room_id, ap_name
+                            ),
                             "map_locations": [loc(entity)],
                             "sections": [{}],
                             "chest_unopened_img": f"images/locations/{heart_color}_heart.png",
