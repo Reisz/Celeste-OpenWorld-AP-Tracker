d08f7aab647160d91c1634e8974c9668031a0c5c
Reisz <Reisz@users.noreply.github.com>
2026-04-12 10:41:34 +0200

    Enable item tracking for keys
    Fixes #41

--- generator/generate_ap_item_mappings.py
+++ generator/generate_ap_item_mappings.py
@@ -6,12 +6,16 @@ from pathlib import Path
 with Path("data/ids.json").open() as f:
     ids = json.load(f)["item_name_to_id"]
 
-with Path("tracker/items/interactables.json").open() as f:
-    interactables = json.load(f)
+ITEM_TYPES = ["interactables", "keys"]
 
 mappings = {}
-for interactable in interactables:
-    mappings[ids[interactable["name"]]] = interactable["codes"]
+
+for item_type in ITEM_TYPES:
+    with Path(f"tracker/items/{item_type}.json").open() as f:
+        items = json.load(f)
+
+    for item in items:
+        mappings[ids[item["name"]]] = item["codes"]
 
 mappings_path = Path("tracker/scripts/mappings")
 mappings_path.mkdir(parents=True, exist_ok=True)
